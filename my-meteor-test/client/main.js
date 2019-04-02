72f368836bc9f4508e89ba76e143155ab19c8ee6
Andrew <andrewthansen@hotmail.com>
2019-04-02 09:50:54 -0500

    Changed more stuff

--- my-meteor-test/client/main.js
+++ my-meteor-test/client/main.js
@@ -2,5 +2,8 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
+import './templates/layout.html';
+import './templates/home.html';
+import './templates/home.md';
+import './templates/about.html';
 
---
+++ my-meteor-test/client/templates/home.md
@@ -0,0 +1,5 @@
+{{#template name="markdownBody"}}
+## Welcome to my Blog
+    Here I'm talking about my latest discoveries
+    from the world of Javascript.
+{{/markdownBody}}
